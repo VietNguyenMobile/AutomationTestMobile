311554b5152d52c473d8a8ebbb6559f6a32717f9
VIET-NGUYEN\Administrator <quocviet.ce@gmail.com>
2021-11-15 22:57:24 +0700

    Add Jenkins

--- src/test/java/com/humanizehealth/testcases/LoginAndCreateNewAccountIOS.java
+++ src/test/java/com/humanizehealth/testcases/LoginAndCreateNewAccountIOS.java
@@ -5,23 +5,24 @@ import com.humanizehealth.pages.AccountSetup;
 import com.humanizehealth.pages.SetUpOption;
 import com.humanizehealth.pages.SignIn;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class LoginAndCreateNewAccountIOS extends Base {
+public class LoginAndCreateNewAccountIOS {
 
-    SignIn signIn;
-    AccountSetup accountSetup;
-    SetUpOption setUpOption;
+//    SignIn signIn;
+//    AccountSetup accountSetup;
+//    SetUpOption setUpOption;
 
-    @Test
-    public void TC_002_Create_New_Account_With_A_Valid_Data_iOS() throws InterruptedException {
-        signIn = new SignIn();
-
-        accountSetup = signIn.clickPlusButton();
-
-        boolean isDisplayed = accountSetup.isTextCompleteYourAccountDisplayed();
-
-        Assert.assertTrue(isDisplayed);
+//    @Test(groups =  {"Smoke"})
+//    public void TC_002_Create_New_Account_With_A_Valid_Data_iOS() throws InterruptedException {
+//        signIn = new SignIn();
+//
+//        accountSetup = signIn.clickPlusButton();
+//
+//        boolean isDisplayed = accountSetup.isTextCompleteYourAccountDisplayed();
+//
+//        Assert.assertTrue(isDisplayed);
 //
 //        String displayName = userTestData.getJSONObject("validAccount").getString("displayName");
 //        String firstName = userTestData.getJSONObject("validAccount").getString("firstName");
@@ -44,8 +45,46 @@ public class LoginAndCreateNewAccountIOS extends Base {
 //                city,
 //                zipCode
 //        );
+//
+//        Assert.assertTrue(true);
+//
+//    }
 
-        Assert.assertTrue(true);
+    @Test
+    public void Start() {
+        System.out.println("Start");
+    }
 
+    @Test(dependsOnMethods = {"Start"})
+    public void Test_cases() {
+
+    }
+
+    @Test(timeOut = 5000)
+    public void Test_case_2() {
+
+    }
+
+    @Test(dataProvider = "getData")
+    public void MobileSignOut(String username, String password) {
+        System.out.println("Mobile SignOut");
+        System.out.println("username " + username);
+        System.out.println("password " + password);
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Object[][] data = new Object[3][2];
+
+        data[0][0] = "firstsetusername";
+        data[0][1] = "firstpassword";
+
+        data[1][0] = "secondsetusername";
+        data[1][1] = "secondpassword";
+
+        data[2][0] = "thirdsetusername";
+        data[2][1] = "thirdpassword";
+
+        return data;
     }
 }
